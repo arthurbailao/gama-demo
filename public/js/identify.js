5956db5f1ae0420335251da16c83def26c435f9e
Arthur Bailao <arthurbailao@gmail.com>
2016-05-20 22:53:11 -0300

    addin identify

---
+++ public/js/identify.js
@@ -0,0 +1,32 @@
+(function(){
+
+  function close() {
+    $('.md-show .md-close').click();
+  }
+
+  function clean() {
+    $('.md-show input[name="name"]').val(''),
+    $('.md-show input[name="email"]').val(''),
+    $('.md-show input[name="phone"]').val('');
+  }
+
+  $('.btn-modal').click(function() {
+    var name = $('.md-show input[name="name"]').val(),
+        email = $('.md-show input[name="email"]').val(),
+        phone = $('.md-show input[name="phone"]').val();
+
+    if (name.trim().length == 0 || email.trim().length == 0 || phone.trim().length == 0) {
+      return;
+    }
+
+    dito.identify({
+      id: dito.Helpers.sha1(email),
+      name: name,
+      data: { telefone: phone }
+    });
+
+    clean();
+    close();
+
+  });
+})();
